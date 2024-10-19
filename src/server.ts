5a8ccfc380b58d07725d04bcb8f6a1668b216571
devAshu12 <ashudev0987@gmail.com>
2024-10-19 10:20:55 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
-function login(username: string): boolean {
-  return true;
-}
+import app from "./app";
+import { Config } from "./config";
+import logger from "./config/logger";
 
-login("Ashutosh");
+const startServer = async () => {
+  const PORT = Config.PORT;
+  try {
+    // throw new Error("Something went wrong");
+    app.listen(PORT, () => {
+      logger.info(`Listining on PORT: ${PORT}`);
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(err.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
+  }
+};
+
+startServer();
